1b4828fc1aca5f372dc74e0b33c26104b482b64e
sosina14 <sosiayu14@gmail.com>
2025-03-15 23:48:48 +0300

    update

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import register, user_login, user_logout
 
-from .views import manage_books, manage_users, edit_user, delete_user
+from .views import manage_books, manage_users, edit_user, delete_user , add_book
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('manage-books/', manage_books, name='manage_books'),
     path('manage-users/', manage_users, name='manage_users'),
+    path('add-book/', add_book, name='add_book'),
     path('edit-user/<int:user_id>/', edit_user, name='edit_user'),
     path('delete-user/<int:user_id>/', delete_user, name='delete_user'),
 ]
